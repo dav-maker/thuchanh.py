dd0dfe7633af2e916529a99ce787016baa8fc820
dav-maker <mnghia172@gmail.com>
2026-04-24 11:13:23 +0700

    Nop bai

--- dequy.py
+++ dequy.py
@@ -25,3 +25,18 @@ def gcd(a, b):
 a = int(input("Nhập a: "))
 b = int(input("Nhập b: "))
 print("UCLN =", gcd(a, b))
+
+def fibonacci(n):
+    # Điểm dừng (Base case): F0 = 1 và F1 = 1
+    if n == 0 or n == 1:
+        return 1
+    # Công thức đệ quy: Fn = Fn-1 + Fn-2
+    else:
+        return fibonacci(n - 1) + fibonacci(n - 2)
+
+# Kiểm tra với các ví dụ trong ảnh
+n1 = 7
+print(f"Với n = {n1}, số Fibonacci thứ {n1} là: {fibonacci(n1)}") # Kết quả: 13
+
+n2 = 8
+print(f"Với n = {n2}, số Fibonacci thứ {n2} là: {fibonacci(n2)}") # Kết quả: 21
